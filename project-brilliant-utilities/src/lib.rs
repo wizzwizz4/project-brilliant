7c28e6a069fcd4d401c2cc7160311e901bf0d7e2
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2019-06-21 22:26:14 +0100

    Added operators, but not all tests.

--- project-brilliant-utilities/src/lib.rs
+++ project-brilliant-utilities/src/lib.rs
@@ -1,21 +1,86 @@
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct NanoSecond(u64);
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Currency(u64);
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Token(u64);
+#[macro_use]
+extern crate derive_more;
+
+use std::ops::{Mul, Div, Rem};
 
 pub const NANOSECONDS_PER_DAY: Token = Token(86_400_000_000_000);
+
+#[derive(Clone, Debug, From, PartialEq, Eq, PartialOrd, Ord, Add, AddAssign)]
+pub struct NanoSecond(u64);
+#[derive(Clone, Debug, From, PartialEq, Eq, PartialOrd, Ord, Add, AddAssign)]
+pub struct Currency(u64);
+#[derive(Clone, Debug, From, PartialEq, Eq, PartialOrd, Ord, Add, AddAssign)]
+pub struct Token(u64);
+
+impl Mul<Token> for NanoSecond {
+    type Output = Currency;
+    fn mul(self: NanoSecond, rhs: Token) -> Currency {
+        Currency(self.0 * rhs.0)
+    }
+}
+impl Mul<&Token> for NanoSecond {
+    type Output = Currency;
+    fn mul(self: NanoSecond, rhs: &Token) -> Currency {
+        Currency(self.0 * rhs.0)
+    }
+}
+impl Mul<NanoSecond> for Token {
+    type Output = Currency;
+    fn mul(self: Token, rhs: NanoSecond) -> Currency {
+        Currency(self.0 * rhs.0)
+    }
+}
+impl Mul<&NanoSecond> for Token {
+    type Output = Currency;
+    fn mul(self: Token, rhs: &NanoSecond) -> Currency {
+        Currency(self.0 * rhs.0)
+    }
+}
+impl Div<NanoSecond> for Currency {
+    type Output = Token;
+    fn div(self: Currency, rhs: NanoSecond) -> Token {
+        Token(self.0 / rhs.0)
+    }
+}
+impl Div<&NanoSecond> for Currency {
+    type Output = Token;
+    fn div(self: Currency, rhs: &NanoSecond) -> Token {
+        Token(self.0 / rhs.0)
+    }
+}
+impl Rem<NanoSecond> for Currency {
+    type Output = Token;
+    fn rem(self: Currency, rhs: NanoSecond) -> Token {
+        Token(self.0 % rhs.0)
+    }
+}
+impl Rem<&NanoSecond> for Currency {
+    type Output = Token;
+    fn rem(self: Currency, rhs: &NanoSecond) -> Token {
+        Token(self.0 % rhs.0)
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn no_typo_in_npd_constant() {
         assert_eq!(
             NANOSECONDS_PER_DAY,
             Token(1_000_000_000 * 60 * 60 * 24)
         )
+    }
+    #[test]
+    fn nanosecond_mul() {
+        for x in 0..64 {
+            for y in 0..64 {
+                assert_eq!(
+                    x * y,
+                    (NanoSecond::from(x) * Token::from(y)).0
+                )
+            }
+        }
     }
 }
