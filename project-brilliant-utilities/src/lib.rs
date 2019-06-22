b437e5f80e53f53be1d0509372d1b824385d3574
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2019-06-22 23:52:23 +0100

    Added the rest of operators and tests.

--- project-brilliant-utilities/src/lib.rs
+++ project-brilliant-utilities/src/lib.rs
@@ -60,10 +60,52 @@ impl Rem<&NanoSecond> for Currency {
         Token(self.0 % rhs.0)
     }
 }
+impl Div<Token> for Currency {
+    type Output = NanoSecond;
+    fn div(self: Currency, rhs: Token) -> NanoSecond {
+        NanoSecond(self.0 / rhs.0)
+    }
+}
+impl Div<&Token> for Currency {
+    type Output = NanoSecond;
+    fn div(self: Currency, rhs: &Token) -> NanoSecond {
+        NanoSecond(self.0 / rhs.0)
+    }
+}
+impl Rem<Token> for Currency {
+    type Output = NanoSecond;
+    fn rem(self: Currency, rhs: Token) -> NanoSecond {
+        NanoSecond(self.0 % rhs.0)
+    }
+}
+impl Rem<&Token> for Currency {
+    type Output = NanoSecond;
+    fn rem(self: Currency, rhs: &Token) -> NanoSecond {
+        NanoSecond(self.0 % rhs.0)
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ops::{Add};
+
+    // Rust: Something's too hard? Butcher it with a macro!
+    macro_rules! sparse_to_32 {
+        () => {
+                   (0..64).step_by(1)
+            .chain((64..65536).step_by(13))
+            .chain((65536..4_294_967_296).step_by(67_867_967))
+        }
+    }
+    macro_rules! sparse_to_64 {
+        () => {
+            sparse_to_32!()
+            .chain((4_294_967_296..=18_446_744_073_709_551_615).step_by(
+                184_467_447_370_955_227
+            ))
+        }
+    }
 
     #[test]
     fn no_typo_in_npd_constant() {
@@ -72,14 +114,79 @@ mod tests {
             Token(1_000_000_000 * 60 * 60 * 24)
         )
     }
+
     #[test]
-    fn nanosecond_mul() {
-        for x in 0..64 {
-            for y in 0..64 {
+    fn test_mul() {
+        // you know what, just test everything at once
+        for x in sparse_to_32!() {
+            for y in sparse_to_32!() {
                 assert_eq!(
-                    x * y,
-                    (NanoSecond::from(x) * Token::from(y)).0
-                )
+                    Currency(x * y),
+                    (NanoSecond::from(x) * Token::from(y)),
+                    "NanoSecond::from({}) * Token::from({})", x, y
+                );
+                assert_eq!(
+                    Currency(x * y),
+                    (NanoSecond::from(x) * &Token::from(y)),
+                    "NanoSecond::from({}) * &Token::from({})", x, y
+                );
+                assert_eq!(
+                    Currency(x * y),
+                    (Token::from(x) * NanoSecond::from(y)),
+                    "Token::from({}) * NanoSecond::from({})", x, y
+                );
+                assert_eq!(
+                    Currency(x * y),
+                    (Token::from(x) * &NanoSecond::from(y)),
+                    "Token::from({}) * &NanoSecond::from({})", x, y
+                );
+            }
+        }
+    }
+    #[test]
+    fn test_div_rem() {
+        for x in sparse_to_64!() {
+            for y in sparse_to_32!().skip(1) {
+                assert_eq!(
+                    Token(x / y),
+                    Currency::from(x) / NanoSecond::from(y),
+                    "Currency::from({}) / NanoSecond::from({})", x, y
+                );
+                assert_eq!(
+                    Token(x / y),
+                    Currency::from(x) / &NanoSecond::from(y),
+                    "Currency::from({}) / &NanoSecond::from({})", x, y
+                );
+                assert_eq!(
+                    Token(x % y),
+                    Currency::from(x) % NanoSecond::from(y),
+                    "Currency::from({}) % NanoSecond::from({})", x, y
+                );
+                assert_eq!(
+                    Token(x % y),
+                    Currency::from(x) % &NanoSecond::from(y),
+                    "Currency::from({}) % &NanoSecond::from({})", x, y
+                );
+                assert_eq!(
+                    NanoSecond(x / y),
+                    Currency::from(x) / Token::from(y),
+                    "Currency::from({}) / Token::from({})", x, y
+                );
+                assert_eq!(
+                    NanoSecond(x / y),
+                    Currency::from(x) / &Token::from(y),
+                    "Currency::from({}) / &Token::from({})", x, y
+                );
+                assert_eq!(
+                    NanoSecond(x % y),
+                    Currency::from(x) % Token::from(y),
+                    "Currency::from({}) % Token::from({})", x, y
+                );
+                assert_eq!(
+                    NanoSecond(x % y),
+                    Currency::from(x) % &Token::from(y),
+                    "Currency::from({}) % &Token::from({})", x, y
+                );
             }
         }
     }
